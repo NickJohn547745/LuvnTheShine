667a765e71fcae194b23c9e69c6d44e62a375b82
Nicholas Johnson <johnson.8777@buckeyemail.osu.edu>
2022-05-23 18:03:52 -0400

    adding recursive files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
+  <v-app>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
+
+<style>
+  @import './assets/styles/main.css';
+</style>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  listeners: {},
+
+  data: () => ({
+    
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,42 @@
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'vuetify/styles'
+
+import { loadFonts } from './plugins/webfontloader'
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+loadFonts();
+
+const Home = { template: '<div>Home</div>' }
+const Products = { template: '<div>Products</div>' }
+const About = { template: '<div>About</div>' }
+const Contact = { template: '<div>Contact</div>' }
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/products', component: Products },
+  { path: '/about', component: About },
+  { path: '/contact', component: Contact },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
